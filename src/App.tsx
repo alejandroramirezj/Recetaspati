5e05769ff363400652aae564065619fc8f9ae2db
Alejandro Ramirez <48692145+alejandroramirezj@users.noreply.github.com>
2025-04-27 22:58:00 +0200

    fix: Add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,19 @@ import CategoryPage from "./pages/CategoryPage";
 
 const queryClient = new QueryClient();
 
+// Define the base path
+const baseName = "/Recetaspati";
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={baseName}>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/product/:category/:id" element={<ProductDetail />} />
-          <Route path="/category/:categoryName" element={<CategoryPage />} />
+          <Route path="product/:category/:id" element={<ProductDetail />} />
+          <Route path="category/:categoryName" element={<CategoryPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
