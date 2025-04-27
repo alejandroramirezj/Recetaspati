9de93618f6c1d895bf409d6bd4527bf5614758aa
Alejandro Ramirez <48692145+alejandroramirezj@users.noreply.github.com>
2025-04-27 16:20:03 +0200

    fix: Configure base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Recetaspati/",
   server: {
     host: "::",
     port: 8080,
